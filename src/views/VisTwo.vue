f3611b21826b23fa1b9adbfb53ca834058c90413
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-04-28 11:26:15 +0200

    map vis

--- src/views/VisTwo.vue
+++ src/views/VisTwo.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- <v-chart class="chart" :option="option" :loading="loading" /> -->
-    <div id="mainn"></div>
+    <div id="main"></div>
 </template>
 
 <script>
@@ -8,6 +8,8 @@ import 'echarts'
 import VChart, { THEME_KEY } from 'vue-echarts'
 import { ref, defineComponent } from 'vue'
 import busLinesJson from '@/assets/lines-bus.json'
+import geoJson from '@/assets/geoJson.json'
+import usaJson from '@/assets/usaJson.json'
 import * as echarts from 'echarts'
 
 export default defineComponent({
@@ -26,199 +28,59 @@ export default defineComponent({
         }
     },
     mounted() {
-        var chartDom = document.getElementById('mainn')
+        var chartDom = document.getElementById('main')
         var myChart = echarts.init(chartDom)
-
-        let hStep = 300 / (busLinesJson.length - 1)
-        let busLines = [].concat.apply(
-            [],
-            busLinesJson.map(function (busLine, idx) {
-                let prevPt = []
-                let points = []
-                for (let i = 0; i < busLine.length; i += 2) {
-                    let pt = [busLine[i], busLine[i + 1]]
-                    if (i > 0) {
-                        pt = [prevPt[0] + pt[0], prevPt[1] + pt[1]]
-                    }
-                    prevPt = pt
-                    points.push([pt[0] / 1e4, pt[1] / 1e4])
-                }
-                return {
-                    coords: points,
-                    lineStyle: {
-                        normal: {
-                            color: echarts.color.modifyHSL('#5A94DF', Math.round(hStep * idx)),
-                        },
-                    },
-                }
-            })
-        )
+        echarts.registerMap('USA', geoJson)
 
         let option = {
-            bmap: {
-                center: [116.46, 39.92],
-                zoom: 10,
-                roam: true,
-                mapStyle: {
-                    styleJson: [
-                        {
-                            featureType: 'water',
-                            elementType: 'all',
-                            stylers: {
-                                color: '#031628',
-                            },
-                        },
-                        {
-                            featureType: 'land',
-                            elementType: 'geometry',
-                            stylers: {
-                                color: '#000102',
-                            },
-                        },
-                        {
-                            featureType: 'highway',
-                            elementType: 'all',
-                            stylers: {
-                                visibility: 'off',
-                            },
-                        },
-                        {
-                            featureType: 'arterial',
-                            elementType: 'geometry.fill',
-                            stylers: {
-                                color: '#000000',
-                            },
-                        },
-                        {
-                            featureType: 'arterial',
-                            elementType: 'geometry.stroke',
-                            stylers: {
-                                color: '#0b3d51',
-                            },
-                        },
-                        {
-                            featureType: 'local',
-                            elementType: 'geometry',
-                            stylers: {
-                                color: '#000000',
-                            },
-                        },
-                        {
-                            featureType: 'railway',
-                            elementType: 'geometry.fill',
-                            stylers: {
-                                color: '#000000',
-                            },
-                        },
-                        {
-                            featureType: 'railway',
-                            elementType: 'geometry.stroke',
-                            stylers: {
-                                color: '#08304b',
-                            },
-                        },
-                        {
-                            featureType: 'subway',
-                            elementType: 'geometry',
-                            stylers: {
-                                lightness: -70,
-                            },
-                        },
-                        {
-                            featureType: 'building',
-                            elementType: 'geometry.fill',
-                            stylers: {
-                                color: '#000000',
-                            },
-                        },
-                        {
-                            featureType: 'all',
-                            elementType: 'labels.text.fill',
-                            stylers: {
-                                color: '#857f7f',
-                            },
-                        },
-                        {
-                            featureType: 'all',
-                            elementType: 'labels.text.stroke',
-                            stylers: {
-                                color: '#000000',
-                            },
-                        },
-                        {
-                            featureType: 'building',
-                            elementType: 'geometry',
-                            stylers: {
-                                color: '#022338',
-                            },
-                        },
-                        {
-                            featureType: 'green',
-                            elementType: 'geometry',
-                            stylers: {
-                                color: '#062032',
-                            },
-                        },
-                        {
-                            featureType: 'boundary',
-                            elementType: 'all',
-                            stylers: {
-                                color: '#465b6c',
-                            },
-                        },
-                        {
-                            featureType: 'manmade',
-                            elementType: 'all',
-                            stylers: {
-                                color: '#022338',
-                            },
-                        },
-                        {
-                            featureType: 'label',
-                            elementType: 'all',
-                            stylers: {
-                                visibility: 'off',
-                            },
-                        },
-                    ],
+            title: {
+                text: 'USA Population Estimates (2012)',
+                subtext: 'Data from www.census.gov',
+                sublink: 'http://www.census.gov/popest/data/datasets.html',
+                left: 'right',
+            },
+            tooltip: {
+                trigger: 'item',
+                showDelay: 0,
+                transitionDuration: 0.2,
+            },
+            visualMap: {
+                left: 'right',
+                min: 500000,
+                max: 38000000,
+                inRange: {
+                    color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026'],
+                },
+                text: ['High', 'Low'],
+                calculable: true,
+            },
+            toolbox: {
+                show: true,
+                //orient: 'vertical',
+                left: 'left',
+                top: 'top',
+                feature: {
+                    dataView: { readOnly: false },
+                    restore: {},
+                    saveAsImage: {},
                 },
             },
             series: [
                 {
-                    type: 'lines',
-                    coordinateSystem: 'bmap',
-                    polyline: true,
-                    data: busLines,
-                    silent: true,
-                    lineStyle: {
-                        // color: '#c23531',
-                        // color: 'rgb(200, 35, 45)',
-                        opacity: 0.2,
-                        width: 1,
+                    name: 'USA PopEstimates',
+                    type: 'map',
+                    roam: true,
+                    map: 'USA',
+                    emphasis: {
+                        label: {
+                            show: true,
+                        },
                     },
-                    progressiveThreshold: 500,
-                    progressive: 200,
-                },
-                {
-                    type: 'lines',
-                    coordinateSystem: 'bmap',
-                    polyline: true,
-                    data: busLines,
-                    lineStyle: {
-                        width: 0,
-                    },
-                    effect: {
-                        constantSpeed: 20,
-                        show: true,
-                        trailLength: 0.1,
-                        symbolSize: 1.5,
-                    },
-                    zlevel: 1,
+                    data: [{ name: 'Northwest', value: 4822023 }],
                 },
             ],
         }
-        // console.log(option)
-        // option && myChart.setOption(option)
+        myChart.setOption(option)
     },
     methods: {
         generateRandomHexColor() {
