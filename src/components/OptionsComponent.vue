1aae7d23b20ef2754deef9e38e7841feb3667112
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-05-05 10:48:01 +0200

    feat: options

---
+++ src/components/OptionsComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Categories</h1>
+        <div class="form-control">
+            <label v-for="c in categories" :key="c" class="label cursor-pointer">
+                <span class="label-text">{{ c }}</span>
+                <input type="checkbox" class="toggle" checked />
+            </label>
+        </div>
+    </div>
+</template>
+
+<script>
+import { useCategoriesStore } from '@/stores/categories'
+import { mapWritableState } from 'pinia'
+
+export default {
+    components: {},
+    props: {},
+    data() {
+        return {}
+    },
+    computed: {
+        ...mapWritableState(useCategoriesStore, ['categories']),
+    },
+    mounted() {},
+    methods: {},
+}
+</script>
