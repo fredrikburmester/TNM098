4db6c934a066dd5c675c4e11eba7fa16c7db7d0d
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-05-30 14:11:25 +0200

    fix: enable all categories by default

--- src/stores/categories.js
+++ src/stores/categories.js
@@ -41,11 +41,11 @@ export const useCategoriesStore = defineStore({
         // Map of selected categories allowing us to have multipile selected at once
         selectedCategories: {
             0: true,
-            1: false,
-            2: false,
-            3: false,
-            4: false,
-            5: false,
+            1: true,
+            2: true,
+            3: true,
+            4: true,
+            5: true,
         },
     }),
     getters: {},
